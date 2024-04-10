f0f45dec0140428f988bfec9d95c08eff1f14f7c
amshul <amshulmp@gmail.com>
2024-04-10 20:59:27 +0530

    search added

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,194 +1,218 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_ease_user/config/configuration.dart';
 import 'package:shop_ease_user/config/routes.dart';
 
-
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController searchcontroller = TextEditingController();
-  Map<String, bool> wishlistStates = {}; 
+  TextEditingController searchController = TextEditingController();
+  bool _showSearchBar = false;
+  List<DocumentSnapshot>? filteredproducts;
+  List<DocumentSnapshot>? products;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<WhishListProvider>(context, listen: false).initializeWishlistStates();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Provider.of<WhishListProvider>(context,listen: false);
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         toolbarHeight: MyConstants.screenHeight(context) * 0.08,
         backgroundColor: Theme.of(context).colorScheme.primary,
+        leading:  LottieBuilder.asset("assets/Animation - 1708494702546.json"),
+        title: Text("SHOP EASE"),
         actions: [
-          Flexible(
-            child: Padding(
-              padding: EdgeInsets.all(MyConstants.screenHeight(context) * 0.004),
-              child: TextFormField(
-                controller: searchcontroller,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  hintStyle: const TextStyle(fontSize: 20),
-                  hintText: "search",
-                  errorBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  suffixIcon: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.search),
-                  ),
-                ),
-              ),
-            ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _showSearchBar = !_showSearchBar;
+              });
+            },
+            icon: const Icon(Icons.search),
           ),
         ],
       ),
       body: Padding(
         padding: EdgeInsets.all(MyConstants.screenHeight(context) * 0.01),
-        child: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection("products").snapshots(),
-          builder: (BuildContext context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            }
-            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-              return const Center(child: Text('No  data found.'));
-            }
+        child: Column(
+          children: [
+            AnimatedCrossFade(
+              duration: const Duration(milliseconds: 300),
+              crossFadeState: _showSearchBar ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+              firstChild: const SizedBox.shrink(),
+              secondChild: TextField(
+                controller: searchController,
+                decoration: const InputDecoration(
+                  hintText: "Search...",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    filteredproducts = fllteredproducts(value);
+                  });
+                },
+              ),
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection("products").snapshots(),
+              builder: (BuildContext context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return const Center(child: Text('No data found.'));
+                }
 
-            final userData = snapshot.data!.docs;
+                products = snapshot.data?.docs;
+                List<DocumentSnapshot> displayproducts =
+                    searchController.text.isEmpty ? products! : filteredproducts ?? products!;
 
-            return Consumer<WhishListProvider>(
-              builder: (context,data,_) {
-                return GridView.builder(
-                  itemCount: userData.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 0.73),
-                  itemBuilder: (BuildContext context, int index) {
-                    data.initializeWishlistStates();
-                    final userDoc = userData[index];
-                    final documentId = userDoc.id;
-                    final Map<String, dynamic> userMap = userDoc.data() as Map<String, dynamic>;
-                    final String image = userMap["imageUrls"]?[0] ?? '';
-                    final String name = userMap["productName"] ?? '';
-                    final double price = userMap["price"] ?? '';
-                    final bool isLiked=data.whishList[documentId]??false;
-                
-                    return GestureDetector(
-                      onTap:() {
-                        Navigator.pushNamed(context, Routes.viewproduct,arguments: {
-                          "id":userMap["productid"],
-                          "email":userMap["userEmail"]
-                        });
-                      },
-                      child: SizedBox(
-                        child: Column(
-                          children: [
-                            Stack(
-                              children: [
-                                SizedBox(
-                                  height: MyConstants.screenHeight(context) * 0.25,
-                                  child: Image.network(image, fit: BoxFit.contain),
-                                ),
-                                Positioned(
-                                  top: MyConstants.screenHeight(context) * 0.01,
-                                  right: MyConstants.screenHeight(context) * 0.01,
-                                  child: Consumer<WhishListProvider>(
-                                    builder: (context,data,_) {
-                                      return IconButton(
-                                        onPressed: () {
-                                          data.updateWishlist(FirebaseAuth.instance.currentUser!.email, documentId);
-                                        },
-                                        icon: isLiked ? const Icon(Icons.favorite, color: Colors.red) : const Icon(Icons.favorite_outline, color: Colors.red),
-                                      );
-                                    }
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                name,
-                                textAlign: TextAlign.left,
-                                style: const TextStyle(fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                "₹ $price",
-                                textAlign: TextAlign.left,
-                                style: const TextStyle(fontWeight: FontWeight.w600),
-                              ),
-                            )
-                          ],
+                return Consumer<WhishListProvider>(
+                  builder: (context, data, _) {
+                    return Expanded(
+                      child: GridView.builder(
+                        itemCount: displayproducts.length,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.73,
                         ),
+                        itemBuilder: (BuildContext context, int index) {
+                          final userDoc = displayproducts[index];
+                          final documentId = userDoc.id;
+                          final Map<String, dynamic> userMap = userDoc.data() as Map<String, dynamic>;
+                          final String image = userMap["imageUrls"]?[0] ?? '';
+                          final String name = userMap["productName"] ?? '';
+                          final double price = userMap["price"] ?? '';
+                          final bool isLiked = data.whishList[documentId] ?? false;
+
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(context, Routes.viewproduct, arguments: {
+                                "id": userMap["productid"],
+                                "email": userMap["userEmail"],
+                              });
+                            },
+                            child: SizedBox(
+                              child: Column(
+                                children: [
+                                  Stack(
+                                    children: [
+                                      SizedBox(
+                                        height: MyConstants.screenHeight(context) * 0.25,
+                                        child: Image.network(image, fit: BoxFit.contain),
+                                      ),
+                                      Positioned(
+                                        top: MyConstants.screenHeight(context) * 0.01,
+                                        right: MyConstants.screenHeight(context) * 0.01,
+                                        child: IconButton(
+                                          onPressed: () {
+                                            data.updateWishlist(FirebaseAuth.instance.currentUser!.email, documentId);
+                                          },
+                                          icon: isLiked ? const Icon(Icons.favorite, color: Colors.red) : const Icon(Icons.favorite_outline, color: Colors.red),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      name,
+                                      textAlign: TextAlign.left,
+                                      style: const TextStyle(fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      "₹ $price",
+                                      textAlign: TextAlign.left,
+                                      style: const TextStyle(fontWeight: FontWeight.w600),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     );
                   },
                 );
-              }
-            );
-          },
+              },
+            ),
+          ],
         ),
       ),
     );
   }
+
+  List<DocumentSnapshot> fllteredproducts(String query) {
+    if (query.isEmpty) {
+      return [];
+    } else {
+      List<DocumentSnapshot> filteredList = [];
+      for (var product in products!) {
+        String productnamename = product["productName"].toLowerCase();
+        if (productnamename.contains(query.toLowerCase())) {
+          filteredList.add(product);
+        }
+      }
+      return filteredList;
+    }
+  }
 }
 
-class WhishListProvider extends ChangeNotifier{
-  Map<String,dynamic> whishListtemp={};
+class WhishListProvider extends ChangeNotifier {
+  Map<String, dynamic> wishListTemp = {};
 
-  Map<String,dynamic> get whishList=>whishListtemp;
+  Map<String, dynamic> get whishList => wishListTemp;
 
   void updateWishlist(String? userEmail, String productId) async {
-    
-      QuerySnapshot snapshot = await FirebaseFirestore.instance.collection("users").where("email", isEqualTo: userEmail).get();
-      var doc = snapshot.docs.first;
-      Map<String, dynamic> userData = doc.data() as Map<String, dynamic>;
+    QuerySnapshot snapshot = await FirebaseFirestore.instance.collection("products").where("email", isEqualTo: userEmail).get();
+    var doc = snapshot.docs.first;
+    Map<String, dynamic> userData = doc.data() as Map<String, dynamic>;
 
-      List<dynamic> wishlist = userData["wishlist"];
+    List<dynamic> wishlist = userData["wishlist"];
 
-      if (wishlist.contains(productId)) {
-        wishlist.remove(productId);
-      } else {
-        wishlist.add(productId);
-      }
+    if (wishlist.contains(productId)) {
+      wishlist.remove(productId);
+    } else {
+      wishlist.add(productId);
+    }
 
-      await doc.reference.update({"wishlist": wishlist});
+    await doc.reference.update({"wishlist": wishlist});
 
-     
-     
-        whishListtemp[productId] = wishlist.contains(productId);
-
-        notifyListeners();
-
+    wishListTemp[productId] = wishlist.contains(productId);
+    notifyListeners();
   }
 
   void initializeWishlistStates() async {
     final userEmail = FirebaseAuth.instance.currentUser!.email;
-    
-      QuerySnapshot snapshot = await FirebaseFirestore.instance.collection("users").where("email", isEqualTo: userEmail).get();
-      var doc = snapshot.docs.first;
-      Map<String, dynamic> userData = doc.data() as Map<String, dynamic>;
-      List<dynamic> wishlist = userData["wishlist"] ?? [];
-      for (var productId in wishlist) {
 
-          whishListtemp[productId] = true; 
-
-      }
-      notifyListeners();
+    QuerySnapshot snapshot = await FirebaseFirestore.instance.collection("products").where("email", isEqualTo: userEmail).get();
+    var doc = snapshot.docs.first;
+    Map<String, dynamic> userData = doc.data() as Map<String, dynamic>;
+    List<dynamic> wishlist = userData["wishlist"] ?? [];
+    for (var productId in wishlist) {
+      wishListTemp[productId] = true;
+    }
+    notifyListeners();
   }
-
 }
-
-// updateWishlist(FirebaseAuth.instance.currentUser!.email, documentId);
